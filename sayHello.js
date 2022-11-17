c5f2bc485f94e0fbdc514ff22c9b86d6772fdb42
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-11-17 18:23:55 -0500

    created js file and added code to debug the code

---
+++ sayHello.js
@@ -0,0 +1,10 @@
+// original codevto debug
+// function sayHello(name) {
+//   return 'Hello'
+// }
+
+
+// had to add hello with an extra string to create space, plus add the parameter (name) so it can return  
+function sayHello(name) {
+  return 'Hello,' + " " + name 
+}
